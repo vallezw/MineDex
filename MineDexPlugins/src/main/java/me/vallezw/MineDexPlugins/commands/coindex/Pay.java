377e888f652fb859f171451204aff139d598c3d7
Valentin Zwerschke <v.zwerschke@mail.de>
2020-07-25 19:43:55 +0000

    Pay command fully working now

--- MineDexPlugins/src/main/java/me/vallezw/MineDexPlugins/commands/coindex/Pay.java
+++ MineDexPlugins/src/main/java/me/vallezw/MineDexPlugins/commands/coindex/Pay.java
@@ -29,16 +29,27 @@ public class Pay implements CommandExecutor {
             }
             Player player = (Player) sender;
             int arglength = args.length;
-            if (arglength != 2){
+            if (arglength != 2){ // Unvalid Arguments
                 player.sendMessage(ChatColor.YELLOW + "/pay" + ChatColor.GRAY + " <player> <amount>");
                 return true;
             }
+
             int amount = Integer.valueOf(args[1]);
+            Player receiver = Bukkit.getPlayer(args[0]);
+
+            if(receiver == null){ // Receiver isnt found or doesnt exist.
+                player.sendMessage(ChatColor.YELLOW + args[0] + ChatColor.RED + " is not online or doesnt exist");
+                return true;
+            }
+            else if (receiver == player){ // Receiver is the Sender
+                player.sendMessage(ChatColor.RED + "You cant pay yourself");
+                return true;
+            }
             DBConnection connection = new DBConnection();
             try {
                 int senderBalance = connection.getCoin(player.getDisplayName());
-                if (senderBalance < amount){
-                    player.sendMessage("You have not enough " + ChatColor.RED + "coindex");
+                if (senderBalance < amount){ // Not enough Balance
+                    player.sendMessage(ChatColor.RED  + "You dont have enough coindex." + ChatColor.GRAY + " You currently have " + ChatColor.DARK_AQUA + senderBalance + " coindex");
                     return true;
                 }
             } catch (SQLException throwables) {
@@ -46,8 +57,12 @@ public class Pay implements CommandExecutor {
             } catch (ClassNotFoundException e) {
                 e.printStackTrace();
             }
-            try {
+            try { // Paying in the database
+                int newSenderBalance = connection.getCoin(player.getDisplayName()) - amount;
+                int newReceiverBalance = connection.getCoin(receiver.getDisplayName()) + amount;
                 connection.payUser(player.getDisplayName(), args[0], amount);
+                player.sendMessage(ChatColor.GRAY + "You payed " + ChatColor.YELLOW + receiver.getDisplayName() + " "+ ChatColor.DARK_AQUA + String.valueOf(amount) + " coindex." + ChatColor.GRAY + " Now you have " + ChatColor.DARK_AQUA + newSenderBalance + " coindex");
+                receiver.sendMessage(ChatColor.YELLOW + player.getDisplayName() +ChatColor.GRAY +  " payed you " + ChatColor.DARK_AQUA +  String.valueOf(amount) + " coindex." + ChatColor.GRAY  + " Now you have " + ChatColor.DARK_AQUA + newReceiverBalance + " coindex");
             } catch (SQLException throwables) {
                 throwables.printStackTrace();
             } catch (ClassNotFoundException e) {
