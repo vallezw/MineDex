0b722f51fed6e15405dc1073e11919290bed317f
Valentin Zwerschke <v.zwerschke@mail.de>
2020-07-25 13:43:20 +0000

    Create empty project

---
+++ MineDexPlugins/src/main/java/me/vallezw/MineDexPlugins/Main.java
@@ -0,0 +1,10 @@
+package me.vallezw.MineDexPlugins;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable(){
+        getLogger().info("MineDex has been enabled.");
+    }
+}
